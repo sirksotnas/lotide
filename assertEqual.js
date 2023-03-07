a2e16ecdd55e757cdc950971e93aa64aa49fa82a
Kris Santos <kris.santos101@gmail.com>
2023-03-06 23:58:56 -0600

    learning console.assert

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+  } else return console.log("🛑🛑🛑Assertion Failed: " + actual + " !=== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);
