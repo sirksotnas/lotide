f0123c5e079004dea48a4f7f5fd1d90a7f3a2870
Kris Santos <kris.santos101@gmail.com>
2023-03-23 21:19:39 -0600

    assertEqual.js file updated

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,16 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑Assertion Failed: ${actual} !=== ${expected}`);
+  let results = "";
+  if (actual !== expected) {
+    results = (`🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    results = (`✅Assertion Passed: ${actual} === ${expected}`);
+  }
+  console.log(results);
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 2);
+assertEqual(1, 1);
