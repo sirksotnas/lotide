67ef2be240eecc86f30ad952b90f1fa417b4acbb
Kris Santos <kris.santos101@gmail.com>
2023-03-27 18:16:07 -0600

    updated head.js file

--- head.js
+++ head.js
@@ -12,5 +12,5 @@ const head = function(actualNumber) {       // function helper that returns the 
 };
 
 // TEST CODE
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
