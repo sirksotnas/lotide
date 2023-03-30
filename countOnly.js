303b5f5aaafcb740c4c05479df3ff73e3c045fca
Kris Santos <kris.santos101@gmail.com>
2023-03-30 15:56:59 -0600

    updated countOnly.js file

--- countOnly.js
+++ countOnly.js
@@ -1,3 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actualNumber, expectedNumber) {
+  if (actualNumber !== expectedNumber) {
+    console.log(`🛑Assertion Failed: ${actualNumber} !== ${expectedNumber}`);
+  } else {
+    console.log(`✅Assertion Passed: ${actualNumber} === ${expectedNumber}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {       // function name countOnly that counts how many instances of each string were found in the allItems array of strings
+  const results = {};                                      // empty object to store values of the object result1
+  for (const item of allItems) {                           // looping over the allItems to check how many times each firstNames appear
+    if (itemsToCount[item]) {                              // conditional statement to only increment our count in results if the item is found in the itemsToCount object
+      if (results[item]) {                                 // conditional statement that increment the count of each item into results as we encounter each string item in the array
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
 const firstNames = [
   "Karl",
   "Salima",
