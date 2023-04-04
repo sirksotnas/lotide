f6c632c52e86d9cb31908bf7a561228a7644b44d
Kris Santos <kris.santos101@gmail.com>
2023-04-04 00:58:29 -0600

    updated letterPosition.js file

--- letterPositions.js
+++ letterPositions.js
@@ -40,11 +40,11 @@ const letterPositions = function(sentence) {
     if (letter === " ") {
       continue;
     }
-    // if the letter is not already a key in the results object, a new key is created with the letter as the key and an empty array as the value.
+    // if the letter is not already a key in the results object, a new key is created with the letter as the key and an empty array as the value
     if (!results[letter]) {
       results[letter] = [];
     }
-    // the index of the current letter is then pushed into the array of indices associated with the current letter in the results object.
+    // the index of the current letter is pushed into the array of indices along with the current letter in the results object
     results[letter].push(index);
   }
   // return the results object
