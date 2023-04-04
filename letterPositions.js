53868f29144ce620abdca75d1aa2341677125800
Kris Santos <kris.santos101@gmail.com>
2023-04-04 00:40:09 -0600

    updated letterPosition.js file

--- letterPositions.js
+++ letterPositions.js
@@ -28,23 +28,24 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-// function name letterPosition that takes in a string and return the index of each character shown in the string
+// function name letterPosition that takes in a string and return the index of each letter shown in the string
 const letterPositions = function(sentence) {
   // create an empty object to store the results
   let results = {};
   // loop through the sentence
   for (let index = 0; index < sentence.length; index++) {
-    const character = sentence[index];
+    // create a variable for each letter in the sentence
+    const letter = sentence[index];
     // skip spaces
-    if (character === " ") {
+    if (letter === " ") {
       continue;
     }
-    // if the character is not already a key in the results object, a new key is created with the character as the key and an empty array as the value.
-    if (!results[character]) {
-      results[character] = [];
+    // if the letter is not already a key in the results object, a new key is created with the letter as the key and an empty array as the value.
+    if (!results[letter]) {
+      results[letter] = [];
     }
-    // the index of the current character is then pushed into the array of indices associated with the current character in the results object.
-    results[character].push(index);
+    // the index of the current letter is then pushed into the array of indices associated with the current letter in the results object.
+    results[letter].push(index);
   }
   // return the results object
   return results;
