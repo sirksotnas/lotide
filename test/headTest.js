06d9832146f5d3f1d230b9693cfda0c32dc4599f
Kris Santos <kris.santos101@gmail.com>
2023-04-16 00:40:55 -0600

    refactored files

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,23 @@
 // import assertEqual function
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 // import head function
 const head = require("../head");
 
 // TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe("head function", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
