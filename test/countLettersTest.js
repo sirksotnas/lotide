a82874c0a4a12fa16f34d37bbb90c2f5c9a98ed5
Kris Santos <kris.santos101@gmail.com>
2023-04-17 04:16:45 -0600

    manually testing countLetters file

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,7 +1,7 @@
-// import assertEqual function 
-const assertEqual = require("../assertEqual")
-// import countLetters function 
-const countLetters = require("../countLetters")
+// import assertEqual function
+const assertEqual = require("../assertEqual");
+// import countLetters function
+const countLetters = require("../countLetters");
 
 console.log(countLetters("lighthouse in the house"));
 const numOfLetters = countLetters("lighthouse in the house");
