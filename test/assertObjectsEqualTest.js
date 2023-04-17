e60c3f7fdc8dcad0aa2a00cd475526d792557329
Kris Santos <kris.santos101@gmail.com>
2023-04-17 04:13:20 -0600

    manually testing assertObjectsEqual file

---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,22 @@
+//import eqObjects function
+const eqObjects = require("../eqObjects")
+//import assertObjectsEqual function
+const assertObjectsEqual = require("../assertObjectsEqual")
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject, anotherShirtObject)); // => true
+assertObjectsEqual(shirtObject, anotherShirtObject);
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+console.log(eqObjects(shirtObject, longSleeveShirtObject)); // => false
+assertObjectsEqual(shirtObject, longSleeveShirtObject);
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+console.log(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject)); // => true
+assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject);
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+console.log(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject)); // => false
+assertObjectsEqual(multiColorShirtObject, longSleeveMultiColorShirtObject);
