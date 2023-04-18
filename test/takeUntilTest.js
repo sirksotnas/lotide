32f4afe8b148cab82350a3eaf1feb7df1c78905f
Kris Santos <kris.santos101@gmail.com>
2023-04-17 20:39:05 -0600

    testing takeUntil file with Mocha and Chai

---
+++ test/takeUntilTest.js
@@ -0,0 +1,14 @@
+// import assertArraysEqual function
+const assertArraysEqual = require('../assertArraysEqual');
+// import takeUntil function
+const takeUntil = require('../takeUntil');
+
+// TEST CODE
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);  // pass!
+assertArraysEqual(results2, ["I've been to Hollywood"]); // fail!
