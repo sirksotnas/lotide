0bd72b901e0f478c89559062351aa14a377ba5ec
Kris Santos <kris.santos101@gmail.com>
2023-04-17 20:41:17 -0600

    manually testing takeUntil file and importing files

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -4,7 +4,7 @@ const assertArraysEqual = require('../assertArraysEqual');
 const takeUntil = require('../takeUntil');
 
 // TEST CODE
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 6];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
